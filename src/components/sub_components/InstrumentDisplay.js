dd17749a82775b5bcfaf7ec93d055483dff18bae
Ju Young Yang <106505763+JuYoung-Yang-00@users.noreply.github.com>
2024-01-30 13:20:05 -0800

    img center, results section re-structured

--- src/components/sub_components/InstrumentDisplay.js
+++ src/components/sub_components/InstrumentDisplay.js
@@ -13,17 +13,17 @@ const InstrumentDisplay = ({
     <div className="instrument-display">
       <div className="instrument-name">{instrumentName}</div>
       <div className="instrument-content">
-        <div className="instrument-row">
+        <div className="column">
           <div className="label">Ground Truth</div>
           <img src={groundTruthImg} alt="Ground Truth" className="image" />
           <audio controls src={groundTruthAudio} className="audio-player">Your browser does not support the audio element.</audio>
         </div>
-        <div className="instrument-row">
+        <div className="column">
           <div className="label">No Modulation</div>
           <img src={noModulationImg} alt="No Modulation" className="image" />
           <audio controls src={noModulationAudio} className="audio-player">Your browser does not support the audio element.</audio>
         </div>
-        <div className="instrument-row">
+        <div className="column">
           <div className="label">Generated</div>
           <img src={generatedImg} alt="Generated" className="image" />
           <audio controls src={generatedAudio} className="audio-player">Your browser does not support the audio element.</audio>
