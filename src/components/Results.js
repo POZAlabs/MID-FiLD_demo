ac845b337fafa6e0b0caee4106a3e3cd87573bf0
Ju Young Yang <106505763+JuYoung-Yang-00@users.noreply.github.com>
2024-02-05 11:54:09 -0800

    new results section, new link

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,41 +2,32 @@ import React from 'react';
 import InstrumentDisplay from './sub_components/InstrumentDisplay';
 
 // Import MIDI image
-import groundTruthCello from '../assets/results/main_melody-string_cello_ground_truth.png';
 import noModulationCello from '../assets/results/main_melody-string_cello_no_modulation.png';
 import generatedCello from '../assets/results/main_melody-string_cello_generated.png';
 
-import groundTruthFlute from '../assets/results/sub_melody-flute-ground_truth.png';
 import noModulationFlute from '../assets/results/sub_melody-flute-no_modulation.png';
 import generatedFlute from '../assets/results/sub_melody-flute_generated.png';
 
-import groundTruthHorn from '../assets/results/sub_melody-horn_ground_truth.png';
 import noModulationHorn from '../assets/results/sub_melody-horn_no_modulation.png';
 import generatedHorn from '../assets/results/sub_melody-horn_generated.png';
 
 // Import Audio
-import groundTruthCelloAudio from '../assets/results/audio/main_melody-string_cello_ground_truth.wav';
 import noModulationCelloAudio from '../assets/results/audio/main_melody-string_cello_no_modulation.wav';
 import generatedCelloAudio from '../assets/results/audio/main_melody-string_cello_generated.wav';
 
-import groundTruthFluteAudio from '../assets/results/audio/sub_melody-flute_ground_truth.wav';
 import noModulationFluteAudio from '../assets/results/audio/sub_melody-flute_no_modulation.wav';
 import generatedFluteAudio from '../assets/results/audio/sub_melody-flute_generated.wav';
 
-import groundTruthHornAudio from '../assets/results/audio/sub_melody-horn_ground_truth.wav';
 import noModulationHornAudio from '../assets/results/audio/sub_melody-horn_no_modulation.wav';
 import generatedHornAudio from '../assets/results/audio/sub_melody-horn_generated.wav';
 
 // Import Spectrogram
-import groundTruthCelloSpectrogram from '../assets/results/spectrograms/main_melody-string_cello-ground_truth.png';
 import noModulationCelloSpectrogram from '../assets/results/spectrograms/main_melody-string_cello-no_modulation.png';
 import generatedCelloSpectrogram from '../assets/results/spectrograms/main_melody-string_cello-generated.png';
 
-import groundTruthFluteSpectrogram from '../assets/results/spectrograms/sub_melody-flute-ground_truth.png';
 import noModulationFluteSpectrogram from '../assets/results/spectrograms/sub_melody-flute-no_modulation.png';
 import generatedFluteSpectrogram from '../assets/results/spectrograms/sub_melody-flute-generated.png';
 
-import groundTruthHornSpectrogram from '../assets/results/spectrograms/sub_melody-horn-ground_truth.png';
 import noModulationHornSpectrogram from '../assets/results/spectrograms/sub_melody-horn-no_modulation.png';
 import generatedHornSpectrogram from '../assets/results/spectrograms/sub_melody-horn-generated.png';
 
@@ -45,15 +36,15 @@ const Results = () => {
     <div id="Results" className='results-section'>
       <h1>Results</h1>
       <div className='results-text'>
-      <p> The tables below compare the differences between ground truth, no modulation, and generated samples through MIDI and audio file. </p>
+      <p> The tables below compare the differences between no modulation and generated samples through MIDI, Mel-Spectrogram, and audio file. </p>
       </div>
       <div className='comparison-container'>
         <InstrumentDisplay
           instrumentName="Cello"
-          groundTruthImg={groundTruthCello}
           noModulationImg={noModulationCello}
+          noModulationSpectrogram={noModulationCelloSpectrogram}
           generatedImg={generatedCello}
-          groundTruthAudio={groundTruthCelloAudio}
+          generatedSpectrogram={generatedCelloSpectrogram}
           noModulationAudio={noModulationCelloAudio}
           generatedAudio={generatedCelloAudio}
         />
@@ -61,10 +52,10 @@ const Results = () => {
       <div>
         <InstrumentDisplay
           instrumentName="Flute"
-          groundTruthImg={groundTruthFlute}
           noModulationImg={noModulationFlute}
+          noModulationSpectrogram={noModulationFluteSpectrogram}
           generatedImg={generatedFlute}
-          groundTruthAudio={groundTruthFluteAudio}
+          generatedSpectrogram={generatedFluteSpectrogram}
           noModulationAudio={noModulationFluteAudio}
           generatedAudio={generatedFluteAudio}
         />
@@ -72,64 +63,13 @@ const Results = () => {
       <div>
         <InstrumentDisplay
           instrumentName="Horn"
-          groundTruthImg={groundTruthHorn}
           noModulationImg={noModulationHorn}
+          noModulationSpectrogram={noModulationHornSpectrogram}
           generatedImg={generatedHorn}
-          groundTruthAudio={groundTruthHornAudio}
+          generatedSpectrogram={generatedHornSpectrogram}
           noModulationAudio={noModulationHornAudio}
           generatedAudio={generatedHornAudio}
         />
-      </div>
-      <h2>Mel-Spectrogram Comparison</h2>
-      <div className='results-text'>
-        <p> We further show differences between ground truth, no modulation, and generated samples through mel-spectrogram below. </p>
-      </div>
-      <div className='spectrogram-container'>
-        <div className="spectrogram-row">
-          <div className="instrument-title">Cello</div>
-          <div className="spectrogram-set">
-            <div className="label">Ground Truth</div>
-            <img src={groundTruthCelloSpectrogram} alt="Cello Ground Truth Spectrogram" />
-          </div>
-          <div className="spectrogram-set">
-            <div className="label">No Modulation</div>
-            <img src={noModulationCelloSpectrogram} alt="Cello No Modulation Spectrogram" />
-          </div>
-          <div className="spectrogram-set">
-            <div className="label">Generated</div>
-            <img src={generatedCelloSpectrogram} alt="Cello Generated Spectrogram" />
-          </div>
-        </div>
-        <div className="spectrogram-row">
-          <div className="instrument-title">Flute</div>
-          <div className="spectrogram-set">
-            <div className="label">Ground Truth</div>
-            <img src={groundTruthFluteSpectrogram} alt="Flute Ground Truth Spectrogram" />
-          </div>
-          <div className="spectrogram-set">
-            <div className="label">No Modulation</div>
-            <img src={noModulationFluteSpectrogram} alt="Flute No Modulation Spectrogram" />
-          </div>
-          <div className="spectrogram-set">
-            <div className="label">Generated</div>
-            <img src={generatedFluteSpectrogram} alt="Flute Generated Spectrogram" />
-          </div>
-        </div>
-        <div className="spectrogram-row">
-          <div className="instrument-title">Horn</div>
-          <div className="spectrogram-set">
-            <div className="label">Ground Truth</div>
-            <img src={groundTruthHornSpectrogram} alt="Horn Ground Truth Spectrogram" />
-          </div>
-          <div className="spectrogram-set">
-            <div className="label">No Modulation</div>
-            <img src={noModulationHornSpectrogram} alt="Horn No Modulation Spectrogram" />
-          </div>
-          <div className="spectrogram-set">
-            <div className="label">Generated</div>
-            <img src={generatedHornSpectrogram} alt="Horn Generated Spectrogram" />
-          </div>
-        </div>
       </div>
     </div>
   );
